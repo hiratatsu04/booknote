6844bd3c5202fbb59baddbf5bb54e62ee1a11065
Tatsuya HIRANO <tatsuya-hirano@msen.jp>
2022-09-14 17:18:19 +0900

    Bookクラスを作成。

---
+++ src/booknote/booknote/Models/Book.vb
@@ -0,0 +1,21 @@
+Option Strict On
+
+Public Class Book
+
+    ' フィールドの定義
+    Private ID As String
+    Public Property Title As String
+    Public Property Author As String
+    Public Property Bookimage As System.Drawing.Image
+    Public Property Genre As String
+    Public Property ReviewValue As Double
+    Public Property Memo As String
+    Public Property BuyDate As Date
+    Public Property StartReadDate As Date
+    Public Property EndReadDate As Date
+
+
+    ' 振る舞いの定義
+
+
+End Class
