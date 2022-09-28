d4b1a124584e2109c84daa3aed10f8e7155bc450
Tatsuya HIRANO <tatsuya-hirano@msen.jp>
2022-09-28 16:06:59 +0900

    プロパティの定義ミスを修正

--- src/booknote/booknote/Models/Book.vb
+++ src/booknote/booknote/Models/Book.vb
@@ -3,10 +3,10 @@ Option Strict On
 Public Class Book
 
     ' フィールドの定義
-    Private ID As String
+    Private ID As Integer
     Public Property Title As String
     Public Property Author As String
-    Public Property Bookimage As System.Drawing.Image
+    Public Property Bookimage As Image
     Public Property Genre As String
     Public Property ReviewValue As Double
     Public Property Memo As String
@@ -15,8 +15,6 @@ Public Class Book
     Public Property EndReadDate As Date
     Public Property RecodeDate As Date
     Public Property UpdateDate As Date
-
-    ' 振る舞いの定義
 
 
 End Class
